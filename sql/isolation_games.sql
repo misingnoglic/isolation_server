6df6af0731b0f6b2fb01ed85eb1cec8479abe10d
Arya Boudaie <arya@aryaboudaie.com>
2024-02-19 21:11:57 -0500

    Add sql

---
+++ sql/isolation_games.sql
@@ -0,0 +1,20 @@
+PRAGMA foreign_keys=ON;
+
+CREATE TABLE IF NOT EXISTS isolationgame (
+    id INT AUTO_INCREMENT,
+    uuid VARCHAR(100) NOT NULL PRIMARY KEY,
+    player1 VARCHAR(100) NOT NULL,
+    player1_secret VARCHAR(100) NOT NULL,
+    player2 VARCHAR(100) DEFAULT '',  /* Allow null for before game starts */
+    player2_secret VARCHAR(100) DEFAULT '',  /* Allow null for before game starts */
+    start_board TEXT NOT NULL,
+    game_status VARCHAR(100) NOT NULL,
+    game_state TEXT DEFAULT '',
+    current_queen VARCHAR(100) DEFAULT '',
+    last_move VARCHAR(100) DEFAULT '',
+    winner VARCHAR(100) DEFAULT '',
+    time_limit INT NOT NULL,
+    epoch_time_limit_next_move FLOAT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at FLOAT
+);
