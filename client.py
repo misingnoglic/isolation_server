ec79616ad275f23343b774e018d7ef1509863758
Arya Boudaie <arya@aryaboudaie.com>
2024-02-19 21:10:13 -0500

    Initial commit

---
+++ client.py
@@ -0,0 +1,165 @@
+import requests
+from test_players import RandomPlayer
+# from submission import CustomPlayer
+import constants
+from server_isolation import Board
+import json
+import time
+import click
+
+URL = 'http://127.0.0.1:5000'
+NEW_GAME = URL + '/game/new'
+GAME_STATUS = URL + '/game/%s'
+MAKE_MOVE = URL + '/game/%s/move'
+JOIN_GAME = URL + '/game/%s/join'
+
+# Set your player class here
+PLAYER_CLASS = RandomPlayer
+
+def test_run():
+    player_1_name = 'player1'
+    player_2_name = 'player2'
+    player_1_secret = ''
+    player_2_secret = ''
+
+
+    new_game = requests.post(NEW_GAME, data={'player_name': player_1_name, 'time_limit': 10, 'start_board': 'CASTLE'})
+    if not new_game.ok:
+        print('Error', new_game.json())
+        return
+    print(new_game.json())
+    game_id = new_game.json()['game_id']
+    player_1_secret = new_game.json()['player_secret']
+    player_1_bot = RandomPlayer(player_1_name, player_1_secret)
+
+    game_status_request = requests.get(GAME_STATUS % game_id)
+    print(game_status_request.json())
+
+    # Create a new player to join the game
+    join_game = requests.post(JOIN_GAME  % game_id, data={'player_name': player_2_name})
+    print(join_game.json())
+    player_2_secret = join_game.json()['player_secret']
+    player_2_bot = RandomPlayer(player_2_name, player_2_secret)
+
+    game_status_request = requests.get(GAME_STATUS % game_id)
+    print(game_status_request.json())
+
+    while True:
+        game_status_request = requests.get(GAME_STATUS % game_id)
+        if game_status_request.json()['game_status'] not in ['in_progress']:
+            print('game over')
+            game = Board.from_json(game_status_request.json()['game_state'])
+            print(game.print_board())
+            print(game_status_request.json())
+            break
+        # Get current player
+        current_player = game_status_request.json()['current_queen']
+        print(current_player)
+        if current_player == player_1_name:
+            player = player_1_bot
+            print('player 1')
+        elif current_player == player_2_name:
+            player = player_2_bot
+            print('player 2')
+        else:
+            raise ValueError('Unknown player')
+        # Make a move
+        game = Board.from_json(game_status_request.json()['game_state'])
+        print(game.print_board())
+        move = player.move(game, 10)
+        print(move)
+        make_move_request = requests.post(
+            MAKE_MOVE % game_id, data={
+                'player_name': player.get_name(), 'player_secret': player.secret, 'move': json.dumps(move), 'client_time': time.time()})
+        print(make_move_request.json())
+        time.sleep(0.5)
+
+
+def host_game(my_name, time_limit):
+    new_game = requests.post(NEW_GAME, data={'player_name': my_name, 'time_limit': int(time_limit), 'start_board': 'CASTLE'})
+    if not new_game.ok:
+        print('Error', new_game.json())
+        return
+    print('Game started, please give this code to your friend and we will start')
+    game_id = new_game.json()['game_id']
+    print(new_game.json()['game_id'])
+    my_secret = new_game.json()['player_secret']
+    agent = PLAYER_CLASS()
+    agent.name = my_name
+
+    while True:
+        game_status_request = requests.get(GAME_STATUS % game_id)
+        if game_status_request.json()['game_status'] != 'need_second_player':
+            break
+        time.sleep(0.5)
+
+    print('game started')
+    play_until_game_is_over(game_id, my_name, my_secret, agent)
+
+
+def play_until_game_is_over(game_id, my_name, my_secret, agent):
+    while True:
+        game_status_request = requests.get(GAME_STATUS % game_id)
+        if game_status_request.json()['game_status'] != 'in_progress':
+            print('Game finished - winner is', game_status_request.json()['winner'])
+            print('Final board:')
+            game = Board.from_json(game_status_request.json()['game_state'])
+            print(game.print_board())
+            break
+
+        if game_status_request.json()['current_queen'] != my_name:
+            # print('not my turn yet')
+            time.sleep(0.5)  # Let's wait for our turn
+            continue
+
+        print('Your turn')
+        game = Board.from_json(game_status_request.json()['game_state'])
+        print(game.print_board())
+        time_left = lambda: 1000 * (game_status_request.json()['time_limit'] - (time.time() - game_status_request.json()['last_move_time']))
+        move = agent.move(game, time_left)
+        print('Time left', time_left())
+        if move is None:
+            raise ValueError('Move is None')
+        print(move)
+        make_move_request = requests.post(
+            MAKE_MOVE % game_id, data={
+                'player_name': my_name, 'player_secret': my_secret, 'move': json.dumps(move), 'client_time': time.time()})
+        if not make_move_request.ok:
+            print('Error', make_move_request.json())
+            return
+        time.sleep(0.5)
+
+
+def join_game(game_id, my_name):
+    join_game = requests.post(JOIN_GAME % game_id, data={'player_name': my_name})
+    print(join_game.json())
+    if not join_game.ok:
+        print('Error', join_game.json())
+        return
+    print(join_game.json())
+    my_secret = join_game.json()['player_secret']
+    agent = PLAYER_CLASS(my_name)
+    print('successfully joined game')
+    play_until_game_is_over(game_id, my_name, my_secret, agent)
+
+
+@click.command()
+# Can either host or join a game
+@click.option('--host', is_flag=True, help='Host a new game')
+@click.option('--join', is_flag=True, help='Join an existing game')
+@click.option('--game_id', help='Game ID to join')
+@click.option('--name', help='Your name')
+@click.option('--time_limit', help='Time limit for each move')
+def main(host, join, game_id, name, time_limit):
+    if host:
+        host_game(name, time_limit)
+    elif join:
+        join_game(game_id, name)
+    else:
+        # print help text
+        print('Please specify --host or --join')
+
+
+if __name__ == '__main__':
+    # test_run()
+    main()
---
+++ server.py
@@ -0,0 +1,219 @@
+import flask
+import uuid
+import sqlite3
+from flask import request
+import random
+import json
+from server_isolation import Board
+import time
+import constants
+
+
+app = flask.Flask(__name__)
+
+# enum for game states
+class GameStatus:
+    NEED_SECOND_PLAYER = 'need_second_player'
+    IN_PROGRESS = 'in_progress'
+    FINISHED = 'finished'
+
+
+class QueenStates:
+    HOST_QUEEN = 'host_queen'
+    GUEST_QUEEN = 'guest_queen'
+
+
+@app.route('/game/new', methods=['POST'])
+def host_game():
+    """Host a new game.
+    POST variables:
+    - player_name: the name of the player
+    - time_limit: the time limit for the game
+    - start_board: JSON string of default board for the game (if None, will be the standard board. If "CASTLE" will be the castle board)
+    """
+    game_id = str(uuid.uuid4())
+    player_secret = str(uuid.uuid4())
+    start_board = request.form['start_board']
+    # Store this game ID in the db
+    conn = sqlite3.connect('sql/isolation.db')
+    c = conn.cursor()
+    c.execute(
+        "INSERT INTO isolationgame (uuid, player1, player1_secret, start_board, game_status, time_limit) VALUES (?, ?, ?, ?, ?, ?)",
+        (game_id, request.form['player_name'], player_secret, start_board, GameStatus.NEED_SECOND_PLAYER, request.form['time_limit']))
+    conn.commit()
+    conn.close()
+    return flask.jsonify({'game_id': game_id, 'player_secret': player_secret})
+
+@app.route('/game/<game_id>/join', methods=['POST'])
+def join_game(game_id):
+    """Join a game.
+    POST variables:
+    - player_name: the name of the player
+    """
+    player_name = request.form['player_name']
+    player_secret = str(uuid.uuid4())
+    conn = sqlite3.connect('sql/isolation.db')
+    c = conn.cursor()
+    c.row_factory = sqlite3.Row
+    # Make sure the game exists and is waiting for a player
+    c.execute("SELECT * FROM isolationgame WHERE uuid = ? AND game_status = ?", (game_id, GameStatus.NEED_SECOND_PLAYER))
+    cur_game = c.fetchone()
+    if cur_game is None:
+        return flask.jsonify({'status': 'error', 'message': 'Game not found or not waiting for a player'}), 400
+    # Get the cur_game's player
+    host_player = cur_game['player1']
+    if host_player == player_name:
+        return flask.jsonify({'status': 'error', 'message': 'You cannot join your own game'}), 400
+
+    first_player = random.choice([player_name, host_player])
+    board = cur_game['start_board']
+    if board == "DEFAULT":
+        board = constants.DEFAULT_BOARD
+    elif board == "CASTLE":
+        board = constants.CASTLE_BOARD
+    else:
+        board = json.loads(board)
+
+
+    new_game = Board(host_player, player_name, board, first_player == host_player)
+    new_game_json = new_game.to_json()
+
+    c.execute(
+        "UPDATE isolationgame SET player2 = ?, player2_secret = ?, game_status = ?, game_state = ?,  current_queen = ?, updated_at = ? WHERE uuid = ? AND game_status = ?",
+        (request.form['player_name'], player_secret, GameStatus.IN_PROGRESS, new_game_json, first_player, time.time(), game_id, GameStatus.NEED_SECOND_PLAYER)
+    )
+
+    conn.commit()
+    conn.close()
+    return flask.jsonify({'player_secret': player_secret, 'first_player': first_player})
+
+
+def _get_game_status(game_id):
+    conn = sqlite3.connect('sql/isolation.db')
+    c = conn.cursor()
+    c.row_factory = sqlite3.Row
+    c.execute("SELECT * FROM isolationgame WHERE uuid = ?", (game_id,))
+    cur_game = c.fetchone()
+    if cur_game is None:
+        return flask.jsonify({'status': 'error', 'message': 'Game not found'}), 400
+    return {
+            'game_status': cur_game['game_status'],
+            'current_queen': cur_game['current_queen'],
+            'player1': cur_game['player1'],
+            'player2': cur_game['player2'],
+            'time_limit': cur_game['time_limit'],
+            'winner': cur_game['winner'],
+            'last_move_time': cur_game['updated_at'],
+            'last_move': cur_game['last_move'],
+            'game_state': cur_game['game_state'],
+        }
+
+@app.route('/game/<game_id>', methods=['GET'])
+def get_game_status(game_id):
+    """Get the state of a game.
+    """
+    game_status = _get_game_status(game_id)
+    if game_status['game_status'] == GameStatus.IN_PROGRESS and game_status['last_move_time'] and game_status['last_move_time'] + game_status['time_limit'] + 5 < time.time():
+        # Update status to finished and set the winner to the other player
+        conn = sqlite3.connect('sql/isolation.db')
+        c = conn.cursor()
+        c.execute("UPDATE isolationgame SET game_status = ?, winner = ? WHERE uuid = ?", (GameStatus.FINISHED, game_status['player1'] if game_status['current_queen'] == game_status['player2'] else game_status['player2'], game_id))
+        conn.commit()
+        conn.close()
+        return flask.jsonify(_get_game_status(game_id))
+    return flask.jsonify(game_status)
+
+
+@app.route('/game/<game_id>/move', methods=['POST'])
+def make_move(game_id):
+    """
+    Make a move in a game.
+    :param game_id:
+    POST variables:
+    - player_name: the name of the player
+    - player_secret: the secret of the player making the move
+    - move: the move to make (JSON string of two coordinates)
+    - client_time: time.time() - the time the client thinks it is when it sends (to account for ping, honor system)
+    :return:
+    - New board state
+    - New game state
+    - Winner (if game is over)
+    """
+
+    # Get the game
+    player_name = request.form['player_name']
+    player_secret = request.form['player_secret']
+    move = request.form['move']
+    if move:
+        move = tuple(json.loads(move))
+    client_time = float(request.form['client_time'])
+    conn = sqlite3.connect('sql/isolation.db')
+    c = conn.cursor()
+    c.row_factory = sqlite3.Row
+    c.execute("SELECT * FROM isolationgame WHERE uuid = ?", (game_id,))
+    cur_game = c.fetchone()
+    if cur_game is None:
+        return flask.jsonify({'status': 'error', 'message': 'Game not found'}), 400
+
+    # Check that the game is in progress or waiting to start
+    if cur_game['game_status'] != GameStatus.IN_PROGRESS:
+        return flask.jsonify({'status': 'error', 'message': 'Game not in progress'}), 400
+
+    # Check that the player is in the game
+    if cur_game['player1'] != player_name and cur_game['player2'] != player_name:
+        return flask.jsonify({'status': 'error', 'message': 'Player not in game'}), 400
+
+    # Check that it is the player's turn, and the secret is correct
+    if (cur_game['current_queen'] == cur_game['player1']):
+        # Test that the current player is the host and the secret is correct
+        other_player = cur_game['player2']
+        if cur_game['player1'] == player_name and cur_game['player1_secret'] != player_secret:
+            return flask.jsonify({'status': 'error', 'message': 'Invalid next player'}), 400
+    else:
+        # Test that the current player is the guest and the secret is correct
+        other_player = cur_game['player1']
+        if cur_game['player2'] == player_name and cur_game['player2_secret'] != player_secret:
+            return flask.jsonify({'status': 'error', 'message': 'Invalid next player'}), 400
+
+    # Check that the timestamp they sent is within 1 second of the server's time
+    if abs(client_time - time.time()) > 1:
+        c.execute("UPDATE isolationgame SET game_status = ?, winner = ? WHERE uuid = ?", (GameStatus.FINISHED, other_player, game_id))
+        return flask.jsonify({'status': 'error', 'message': 'I don\'t believe your timestamp...'}), 400
+
+    # Check their timestamp is within the time limit
+    if client_time - cur_game['updated_at'] > cur_game['time_limit']:
+        # Set the game as finished
+        c.execute("UPDATE isolationgame SET game_status = ?, winner = ? WHERE uuid = ?", (GameStatus.FINISHED, other_player, game_id))
+        return flask.jsonify({'status': 'error', 'message': 'You took too long!'}), 400
+
+    # Create the board based on the game state and make the move
+    board = Board.from_json(cur_game['game_state'])
+    # Check that the move is legal
+    if move not in board.get_player_moves(player_name):
+        c.execute("UPDATE isolationgame SET game_status = ?, winner = ? WHERE uuid = ?", (GameStatus.FINISHED, other_player, game_id))
+        return flask.jsonify({'status': 'error', 'message': 'Illegal move'}), 400
+
+    game_over, winner = board.__apply_move__(move)
+    new_game_state = board.to_json()
+    new_game_status = GameStatus.FINISHED if game_over else GameStatus.IN_PROGRESS
+    # Update the game state
+    c.execute("UPDATE isolationgame SET game_state = ?, game_status = ?, current_queen = ?, updated_at = ?, last_move = ?, winner = ?, updated_at = ? WHERE uuid = ?", (new_game_state, new_game_status, other_player, time.time(), json.dumps(move), winner, time.time(), game_id))
+    conn.commit()
+    conn.close()
+    return flask.jsonify(_get_game_status(game_id))
+
+
+def setup_db_first_time():
+    conn = sqlite3.connect('sql/isolation.db')
+    c = conn.cursor()
+    # Run the SQL command in sql/isolation_games.sql
+    with open('sql/isolation_games.sql', 'r') as f:
+        sql = f.read()
+        c.executescript(sql)
+    conn.commit()
+    conn.close()
+
+
+if __name__ == '__main__':
+    setup_db_first_time()
+    app.run()
