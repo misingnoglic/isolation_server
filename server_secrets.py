42af29736eb6273f16a2e5e4532a4eae09f6a2e3
Arya Boudaie <arya@aryaboudaie.com>
2024-03-10 00:34:36 -0800

    Try constructing the DB URL

--- server_secrets.py
+++ server_secrets.py
@@ -3,4 +3,4 @@ import os
 ANNOUNCEMENT_WEBHOOK_URL = os.environ.get('ANNOUNCEMENT_WEBHOOK_URL', '')
 GAME_WEBHOOK_URL = os.environ.get('GAME_WEBHOOK_URL', '')
 DISCORD_CHANNEL_ID = os.environ.get('DISCORD_CHANNEL_ID', '')
-DB_URL = os.environ.get('DB_URL', '')
+DB_URL = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}:{os.environ['RDS_PORT']}/{os.environ['RDS_DB_NAME']}"
